b3fe97deb00f31a4af3e606ee843249128212b04
Alex Recker <alex@reckerfamily.com>
2015-12-08 21:32:15 -0600

    Adds config.org and wires it up.

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
 (package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+
+(when (not (cl-remove-if-not 
+	    (lambda (p) (equal 'org (car p)))
+	    package-alist))
+  (message "No org-mode package found; installing now...")
+  (package-install 'org))
+
+(org-babel-load-file "~/.emacs.d/config.org")

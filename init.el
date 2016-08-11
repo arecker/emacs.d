a894d231106a7c339fd9d0171d3875b265c4715e
Alex Recker <arecker@healthgrades.com>
2016-08-11 10:16:00 -0500

    Gets rid of org repo for now

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")
-	("org" . "http://orgmode.org/elpa/")))
+	("melpa" . "https://melpa.org/packages/")))
 
 (require 'package)
 (package-initialize)

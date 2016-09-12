c6f03f22372bbc93eaa6688c1865c8d36d4426c1
Alex Recker <arecker@healthgrades.com>
2016-09-12 09:02:45 -0500

    Helm and other things

--- init.el
+++ init.el
@@ -12,17 +12,3 @@
 (recker/package-init)
 
 (org-babel-load-file "~/.emacs.d/README.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (expand-region comment-dwim-2 git-gutter magit use-package org-plus-contrib))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

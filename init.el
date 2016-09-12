99c677bfe12fca1b1dd22506a2e2d840afb8fb7a
Alex Recker <alex@reckerfamily.com>
2016-09-12 00:10:52 -0500

    REBOOOOOOOT

--- init.el
+++ init.el
@@ -1,12 +1,28 @@
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")))
+(defun recker/package-init()
+  (setq package-archives
+	'(("melpa" . "https://melpa.org/packages/")
+	  ("org" . "http://orgmode.org/elpa/")))
 
-(require 'package)
-(package-initialize)
+  (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package)))
+
+(recker/package-init)
 
 (org-babel-load-file "~/.emacs.d/README.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (expand-region comment-dwim-2 git-gutter magit use-package org-plus-contrib))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

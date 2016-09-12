cdec1409673d7abd5fefe4c6f18d1cd2fe90d439
Alex Recker <arecker@healthgrades.com>
2016-09-12 13:45:48 -0500

    Removes package sillyness

--- init.el
+++ init.el
@@ -12,17 +12,3 @@
 (recker/package-init)
 
 (org-babel-load-file "~/.emacs.d/README.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode whitespace-cleanup-mode company git-gutter magit helm-projectile helm expand-region comment-dwim-2 exec-path-from-shell use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

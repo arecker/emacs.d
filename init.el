9c619b715c624834225f5608217a204c33f6af58
Alex Recker <alex@reckerfamily.com>
2019-07-11 12:29:33 -0500

    the big rewrite

--- init.el
+++ init.el
@@ -4,9 +4,6 @@
       '(("gnu" . "https://elpa.gnu.org/packages/")
 	("melpa" . "https://melpa.org/packages/")))
 
-(setq debug-on-error 't
-      network-security-level 'low)
-
 (defun recker/package-init ()
   "Initialize the package manager and install use-package."
   (package-initialize)
@@ -23,3 +20,17 @@
 
 (recker/package-init)
 (recker/load-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (request magit pass terraform-mode editorconfig yaml-mode exec-path-from-shell use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

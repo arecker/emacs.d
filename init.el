e9e43af718e4d59516a8da518f46a66027b4dcea
Alex Recker <alex@reckerfamily.com>
2019-09-27 09:00:47 -0500

    oh boy i hope nothing bad happens

--- init.el
+++ init.el
@@ -20,17 +20,3 @@
 
 (recker/package-init)
 (recker/load-config)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (request magit pass terraform-mode editorconfig yaml-mode exec-path-from-shell use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

f091ed28d85ea28287678d8e15e7ad385d4237fa
Alex Recker <alex@reckerfamily.com>
2021-03-25 23:19:09 -0500

    Starting new config

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")))
+        ("melpa" . "https://melpa.org/packages/")))
 
 (defun recker/package-init ()
   "Initialize the package manager and install use-package."
@@ -15,9 +15,10 @@
   "Tangle configuration and load it."
   (let ((config (concat (file-name-as-directory user-emacs-directory) "README.org")))
     (if (file-exists-p config)
-	(org-babel-load-file config)
+        (org-babel-load-file config)
       (warn (concat config " not found - not loading")))))
 
 (recker/package-init)
 (recker/load-config)
+(org-babel-load-file (concat (file-name-as-directory user-emacs-directory) "new.org"))
 (put 'upcase-region 'disabled nil)

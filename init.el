3c893f0d3f06e83fd380c58ef24128d29e8ae299
Alex Recker <alex@reckerfamily.com>
2022-02-11 11:29:19 -0600

    lol already sick of LSP

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 
 (setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
+      '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 
 (defun recker/package-init ()

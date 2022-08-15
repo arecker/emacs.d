6b475f205a9348fd211068635a6d0891c32ab4f5
Alex Recker <alex@reckerfamily.com>
2022-08-15 11:14:17 -0500

    More stuff

--- init.el
+++ init.el
@@ -20,16 +20,18 @@
 
 (recker/package-init)
 (recker/load-config)
+(put 'upcase-region 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui kubernetes lsp-mode yaml-mode writegood-mode web-mode use-package terraform-mode swiper smex slime-company rich-minority projectile prettier-js plantuml-mode nginx-mode mediawiki markdown-mode magit lua-mode jsonnet-mode idomenu ido-vertical-mode htmlize haskell-mode groovy-mode flycheck expand-region exec-path-from-shell emmet-mode elpy editorconfig dockerfile-mode dictionary deft d-mode company-go bbdb)))
+   '(rust-mode zenburn-theme yaml-mode writegood-mode web-mode use-package twittering-mode swiper spacemacs-theme smex slime-company rich-minority projectile prettier-js plantuml-mode ox-jira nginx-mode monokai-theme mediawiki magit lua-mode lsp-ui lsp-python-ms lsp-pyright kubernetes jsonnet-mode idomenu ido-vertical-mode htmlize haskell-mode groovy-mode flycheck fireplace expand-region exec-path-from-shell emmet-mode elpy eglot editorconfig edit-indirect doom-themes dockerfile-mode dictionary dhall-mode deft dap-mode d-mode csv-mode company-terraform company-go bbdb bats-mode autumn-light-theme async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)

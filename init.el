7dbcfef7d5608879de051a4f6f9b092b7fdcc940
Alex Recker <alex@reckerfamily.com>
2022-09-21 21:48:57 -0500

    lol

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rust-mode zenburn-theme yaml-mode writegood-mode web-mode use-package twittering-mode swiper spacemacs-theme smex slime-company rich-minority projectile prettier-js plantuml-mode ox-jira nginx-mode monokai-theme mediawiki magit lua-mode lsp-ui lsp-python-ms lsp-pyright kubernetes jsonnet-mode idomenu ido-vertical-mode htmlize haskell-mode groovy-mode flycheck fireplace expand-region exec-path-from-shell emmet-mode elpy eglot editorconfig edit-indirect doom-themes dockerfile-mode dictionary dhall-mode deft dap-mode d-mode csv-mode company-terraform company-go bbdb bats-mode autumn-light-theme async)))
+   '(git-timemachine nftables-mode rust-mode zenburn-theme yaml-mode writegood-mode web-mode use-package twittering-mode swiper spacemacs-theme smex slime-company rich-minority projectile prettier-js plantuml-mode ox-jira nginx-mode monokai-theme mediawiki magit lua-mode lsp-ui lsp-python-ms lsp-pyright kubernetes jsonnet-mode idomenu ido-vertical-mode htmlize haskell-mode groovy-mode flycheck fireplace expand-region exec-path-from-shell emmet-mode elpy eglot editorconfig edit-indirect doom-themes dockerfile-mode dictionary dhall-mode deft dap-mode d-mode csv-mode company-terraform company-go bbdb bats-mode autumn-light-theme async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

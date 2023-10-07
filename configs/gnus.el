ef48548e91673ae335ecca553cc8d8ec29c77dd4
Alex Recker <alex@reckerfamily.com>
2023-10-07 12:07:39 -0500

    GNus

--- configs/gnus.el
+++ configs/gnus.el
@@ -1,10 +1,29 @@
-(setq auth-sources (list (concat user-emacs-directory "authinfo.gpg")))
+;; hide startup files in .emacs.d/gnus/
+;; little known fact, there are an infinite number of gnus directories
+;; and they WILL make their way to your home directory whether you
+;; want it or not
+(let ((gnus-dir (concat user-emacs-directory "gnus/")))
+  (setq gnus-startup-file (concat gnus-dir "newsrc"))
+  (setq gnus-home-directory (concat gnus-dir "gnus")
+        nnfolder-directory (concat gnus-dir "gnus/Mail/archive")
+        message-directory (concat gnus-dir "gnus/Mail")
+        nndraft-directory (concat gnus-dir "gnus/Drafts")
+        gnus-cache-directory (concat gnus-dir "gnus/cache")))
 
+;; If you experience dribble, talk to your doctor.
+(setq gnus-use-dribble-file nil)
+
+;; set primary method to empty so the program doesn't absolutely
+;; EXPLODE when you open it
 (setq gnus-select-method '(nnml ""))
-(setq gnus-secondary-select-methods '())
+
+;; set topic mode (the only readable mode) as the default
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
-;; powerful placebo settings
+;; Don't move archived messages anywhere
+(setq gnus-message-archive-group nil)
+
+;; powerful placebo settings for faster perceived speed
 (setq gnus-asynchronous t)
 (setq gnus-use-cache t)
 (setq gnus-check-new-newsgroups nil
@@ -14,11 +33,12 @@
       gnus-nov-is-evil nil)
 (setq gnus-check-new-newsgroups nil
       gnus-use-adaptive-scoring nil)
-(setq gnus-summary-line-format "%U%R%z%I%(%[%4L: %-23,23f%]%) %s")
 
-(setq gnus-use-dribble-file nil)
-(setq gnus-message-archive-group nil)
+;; Look at this fucking variable lol
+(setq gnus-summary-line-format "%U%R%z%I%(%[%4L: %-23,23f%]%) %s
+")
 
+;; Use this nerdy bullshit to save email addresseses for autocompletion
 (use-package bbdb
   :ensure t
   :config (setq bbdb-file (concat user-emacs-directory "bbdb.el"))
@@ -27,7 +47,12 @@
   (setq bbdb-mua-auto-update-p 'query)
   (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus))
 
+;; auto filled messages look like shit on most normal mail clients, so
+;; just turn it off to appease all the filthy casuals I email
 (add-hook 'message-mode-hook #'turn-off-auto-fill)
+
+;; Finally, we're setting up IMAP accounts.
+(setq gnus-secondary-select-methods '())
 
 (add-to-list 'gnus-secondary-select-methods
              '(nnimap "alex@reckerfamily.com"
@@ -37,7 +62,24 @@
                       (nnmail-expiry-target "nnimap+alex@reckerfamily.com:[Gmail]/All Mail")
                       (nnmail-expiry-wait immediate)))
 
+;; not working yet...
+;; (add-to-list 'gnus-secondary-select-methods
+;;              '(nnimap "arecker@zendesk.com"
+;;                       (nnimap-user "arecker@zendesk.com")
+;;                       (nnimap-address "imap.gmail.com")
+;;                       (nnimap-server-port "imaps")
+;;                       (nnimap-stream ssl)
+;;                       (nnmail-expiry-target "nnimap+arecker@zendesk.com:[Gmail]/All Mail")
+;;                       (nnmail-expiry-wait immediate)))
+
+;; sending mail (does this by shelling out to msmpt, which will look
+;; at the sender address to figure out which outgoing server to use
 (setq smtpmail-smtp-service 587
       smtpmail-smtp-user "alex@reckerfamily.com"
       smtpmail-smtp-server "smtp.gmail.com"
       send-mail-function 'smtpmail-send-it)
+(setq message-send-mail-function 'message-send-mail-with-sendmail)
+(setq sendmail-program "msmtp")
+(setq mail-host-address "smtp.gmail.com")
+(setq message-sendmail-f-is-evil 't)
+(setq message-sendmail-extra-arguments '("--read-envelope-from"))

4f340290451970f2a5ee63ce1cb43befce1f8239
Alex Recker <alex@reckerfamily.com>
2023-10-29 01:03:22 -0500

    capture templates

--- configs/org/capture.el
+++ configs/org/capture.el
@@ -14,24 +14,21 @@
 
 (defun recker/build-blog-capture ()
   `("b"
-   "Blog"
-   plain
-   (file ,(expand-file-name (format-time-string "~/src/blog/entries/%Y-%m-%d.html")))
-   ,(format-time-string "<!-- meta:title -->\n<!-- meta:banner %Y-%m-%d.jpg -->\n\n%?")
-   :immediate-finish t
-   :jump-to-captured t))
+    "Blog"
+    plain
+    (file ,(expand-file-name (format-time-string "~/src/blog/entries/%Y-%m-%d.html")))
+    ,(format-time-string "<!-- meta:title -->\n<!-- meta:banner %Y-%m-%d.jpg -->\n\n%?")
+    :immediate-finish t
+    :jump-to-captured t))
 
 (defun recker/build-capture-templates ()
-  (let* ((org org-directory)
-         (work (list 'file (concat org "/work.org.gpg")))
-         (hack (list 'file (concat org "/hack.org"))))
-    `(,(recker/build-opsat-today-capture)
-      ,(recker/build-opsat-task-capture)
-      ("w" "Work" entry ,work "* TODO %?\n" :empty-lines 1)
-      ,(recker/build-blog-capture)
-      ("h" "Hack" entry ,hack "* TODO %? [/]\n" :empty-lines 1)
-      ("m" "Therapy" plain (file+olp+datetree ,(concat org-directory "/therapy.org.gpg")))
-      ("n" "Notes (work)" entry ,work "* %?\n" :empty-lines 1 :jump-to-captured t))))
+  `(,(recker/build-opsat-today-capture)
+    ,(recker/build-opsat-task-capture)
+    ("w" "Work" entry (file ,(concat org-directory "/work.org.gpg") "* TODO %?\n" :empty-lines 1))
+    ,(recker/build-blog-capture)
+    ("h" "Hack" entry (file ,(concat org-directory "/hack.org")) "* TODO %? [/]\n" :empty-lines 1)
+    ("m" "Therapy" plain (file+olp+datetree ,(concat org-directory "/therapy.org.gpg")))
+    ("n" "Notes (work)" entry (file ,(concat org-directory "/work.org.gpg")) "* %?\n" :empty-lines 1 :jump-to-captured t)))
 
 (setq org-capture-templates (recker/build-capture-templates))
 (global-set-key (kbd "C-c c") 'org-capture)

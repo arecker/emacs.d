89c476c2cdbf57ae96c5d9fdf9b7adb435cb27db
Alex Recker <alex@reckerfamily.com>
2023-10-30 21:49:47 -0500

    Simplify capture templates

--- configs/org/capture.el
+++ configs/org/capture.el
@@ -1,48 +1,26 @@
 (defun recker/opsat-generate-todays-template ()
   (format-time-string "* TODO %A, %B %d %Y [/]\nDEADLINE: <%Y-%m-%d %a>\n%?"))
 
-(defun recker/build-opsat-today-capture ()
-  `("o" "Today"
-    entry
-    (file+olp+datetree ,(concat org-directory "/opsat.org"))
-    (function recker/opsat-generate-todays-template)
-    :tree-type month
-    :immediate-finish t))
-
 (defun recker/opsat-find-todays-capture ()
   (goto-char (org-find-exact-headline-in-buffer (format-time-string "%A, %B %d %Y") (current-buffer) t))
   (org-end-of-subtree))
 
-(defun recker/build-opsat-task-capture ()
-  `("t" "Task"
-    checkitem
-    (file+function ,(concat org-directory "/opsat.org") recker/opsat-find-todays-capture)
-    nil))
-
-(defun recker/find-blog-capture ()
+(defun recker/blog-target ()
   "Opens today's blog entry."
   (find-file (expand-file-name (format-time-string "~/src/blog/entries/%Y-%m-%d.html")))
-  (goto-char (point-min))
-  (insert (format-time-string "<!-- meta:title -->\n<!-- meta:banner %Y-%m-%d.jpg -->\n\n"))
-  (goto-char (point-max)))
+  (goto-char (point-min)))
 
-(defun recker/build-blog-capture ()
-  `("b"
-    "Blog"
-    plain
-    (function recker/find-blog-capture)
-    ""
-    :immediate-finish t
-    :jump-to-captured t))
+(defun recker/blog-template ()
+  "Return the metadata for today's blog post."
+  (format-time-string "<!-- meta:title -->\n<!-- meta:banner %Y-%m-%d.jpg -->\n\n"))
 
-(defun recker/build-capture-templates ()
-  `(,(recker/build-opsat-today-capture)
-    ,(recker/build-opsat-task-capture)
-    ("w" "Work" entry (file ,(concat org-directory "/work.org.gpg") "* TODO %?\n" :empty-lines 1))
-    ,(recker/build-blog-capture)
-    ("h" "Hack" entry (file ,(concat org-directory "/hack.org")) "* TODO %? [/]\n" :empty-lines 1)
-    ("m" "Therapy" plain (file+olp+datetree ,(concat org-directory "/therapy.org.gpg")))
-    ("n" "Notes (work)" entry (file ,(concat org-directory "/work.org.gpg")) "* %?\n" :empty-lines 1 :jump-to-captured t)))
+(setq org-capture-templates
+      '(("o" "Today" entry (file+olp+datetree "opsat.org") (function recker/opsat-generate-todays-template) :tree-type month :immediate-finish t)
+        ("t" "Task" checkitem (file+function "opsat.org" recker/opsat-find-todays-capture))
+        ("w" "Work" entry (file "work.org.gpg") "* TODO %?\n" :empty-lines 1)
+        ("b" "Blog" plain (function recker/blog-target) (function recker/blog-template) :immediate-finish t :jump-to-captured t)
+        ("h" "Hack" entry (file "hack.org") "* TODO %? [/]\n" :empty-lines 1)
+        ("m" "Therapy" plain (file+olp+datetree "therapy.org.gpg"))
+        ("n" "Notes (work)" entry (file "work.org.gpg") "* %?\n" :empty-lines 1 :jump-to-captured t)))
 
-(setq org-capture-templates (recker/build-capture-templates))
 (global-set-key (kbd "C-c c") 'org-capture)

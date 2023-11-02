7132a6f30f9c6d78a706181210bc610adf915087
Alex Recker <alex@reckerfamily.com>
2023-11-02 11:24:02 -0500

    org: simplify capture

--- configs/org/capture.el
+++ configs/org/capture.el
@@ -1,6 +1,3 @@
-(defun recker/opsat-template ()
-  (format-time-string "* TODO %? [/]\nDEADLINE: <%Y-%m-%d %a>\n"))
-
 (defun recker/blog-target ()
   "Opens today's blog entry."
   (find-file (expand-file-name (format-time-string "~/src/blog/entries/%Y-%m-%d.html")))
@@ -11,11 +8,12 @@
   (format-time-string "<!-- meta:title -->\n<!-- meta:banner %Y-%m-%d.jpg -->\n\n"))
 
 (setq org-capture-templates
-      '(("o" "Task" entry (file+olp+datetree "opsat.org") (function recker/opsat-template))
+      '(("o" "Task" entry (file+olp+datetree "opsat.org") "* TODO %^{Task}\nDEADLINE: %t\n" :immediate-finish t)
         ("b" "Blog" plain (function recker/blog-target) (function recker/blog-template) :immediate-finish t :jump-to-captured t)
-        ("h" "Hack" entry (file+olp+datetree "hack.org") "* TODO %? [/]\n")
+        ("h" "Hack" entry (file+olp+datetree "hack.org") "* TODO %^{Task}\n%?")
         ("m" "Therapy" plain (file+olp+datetree "therapy.org.gpg"))
-        ("w" "Work" entry (file+olp+datetree "work.org.gpg") "* TODO %?\n" :jump-to-captured t)
-        ("B" "Bible" entry (file+olp+datetree "bible.org") "* %?" :jump-to-captured t)))
+        ("w" "Work" entry (file+olp+datetree "work.org.gpg") "* TODO %^{Task}\n%?")
+        ("W" "Meeting" entry (file+olp+datetree "work.org.gpg") "* %^{Meeting title}\nSCHEDULED: %T\n%?" :jump-to-captured t)
+        ("B" "Bible" plain (file+olp+datetree "bible.org") "" :jump-to-captured t)))
 
 (global-set-key (kbd "C-c c") 'org-capture)

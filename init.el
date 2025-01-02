9f00aecf955079f9fcad15baf28256712363c484
Alex Recker <alex@reckerfamily.com>
2025-01-02 11:09:09 -0600

    Finished fixing up config

--- init.el
+++ init.el
@@ -8,24 +8,3 @@
 ;; We're going back to ONE GIANT README
 ;; all gas no brakes baby
 (recker/load-config)
-
-;; set user info
-(setq user-full-name "Alex Recker")
-(setq user-mail-address "alex@reckerfamily.com")
-
-;; load custom functions
-(let ((functions-file (concat user-emacs-directory "configs/functions.el")))
-  (load-file functions-file)
-  (recker/log "loaded custom functions (%s)" functions-file))
-
-;; load the configs from configs/*.el
-(recker/load-configs '("shell"
-                       "langs"
-                       "go"
-                       "python"
-                       "org"))
-
-;; start the emacs server
-(server-start)
-
-(global-visual-line-mode)
